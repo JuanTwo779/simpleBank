cee01a9eb5429ed4c98bd654937b90ea8e00f3ba
juanm <juanmangubat2@gmail.com>
2022-11-01 01:25:49 +1100

    deposit method complete

--- src/Account.java
+++ src/Account.java
@@ -4,39 +4,44 @@ public class Account {
 
     private String name;
     private String accNum;
-    private int balance;
+    private double balance;
+    private String previousTransaction;
     Scanner scanner;
 
     public Account (String name, String accNum){
+        scanner = new Scanner(System.in);
         balance = 0;
         this.name = name;
         this.accNum = accNum;
     }
 
-    private void menu(){
+    public void menu(){
         char choice = '\0';
         boolean flag = true;
         //scanner = new Scanner(System.in);
         System.out.println("Welcome " + name);
         System.out.println("Your account number is: " + accNum + "\n");
         System.out.println("What would you like to do?" + "\n");
-        System.out.println("A. Show balance" + "\n" + "B. Deposit money" + "\n" +
+        
+        do {
+            System.out.println("");
+            System.out.println("A. Show balance" + "\n" + "B. Deposit money" + "\n" +
                 "C. Withdraw money" + "\n" + "D. View previous transaction" + "\n" +
                 "E. Exit" + "\n");
-
-        do {
             System.out.println("Enter an option: ");
+
             choice = scanner.next().charAt(0);
             choice = Character.toUpperCase(choice);
             if (choice == 'A'){
                 balance();
             } else if (choice == 'B') {
-
+                deposit();
             } else if (choice == 'C') {
 
             } else if (choice == 'D') {
 
             } else if (choice == 'E') {
+                System.out.println("Exited successfully");
                 flag = false;
             } else {
                 System.out.println("Invalid");
@@ -52,16 +57,31 @@ public class Account {
     }
 
     private void deposit(){
+        System.out.println("**********************************************");
         System.out.println("Enter the amount you wish to deposit:");
-        boolean flag = true;
-        int value;
-        value = Integer.parseInt(scanner.nextLine());
+        System.out.println("**********************************************");
+        scanner = new Scanner(System.in);
+        double value;
 
-        while (flag){
-            if (value )
+        if (scanner.hasNextDouble()){
+            value = Double.parseDouble(scanner.nextLine());
+            System.out.println("\n" + "$" + value + " successfully deposited");
+            previousTransaction = "Previous balance was: " + balance + "\n" 
+                                    + "$" + value + " was deposited";
+            balance = balance + value;
         }
+        else{
+            System.out.println("User input requires an number value" + "\n");
+            deposit();
+        }
+    }
 
-        balance = balance + value;
+    private void withdraw(){
+        System.out.println("**********************************************");
+        System.out.println("Enter the amount you wish to withdraw");
+        System.out.println("**********************************************");
+        scanner = new Scanner(System.in);
+        double value;
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        Account Juan = new Account("Juan Mangubat", "A12345");
+        Juan.menu();
 
     }
 }
